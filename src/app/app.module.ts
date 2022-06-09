a39d7e2e96d8829e92287b80f33c9fdb908aa861
RolandTapie <kenloth1@yahoo.fr>
2022-06-09 11:22:32 +0200

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Composant1Component } from './composant1/composant1.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, Composant1Component],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
